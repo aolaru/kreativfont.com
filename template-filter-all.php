868f702d324c1afe18a9427d2369b5fa9463fdd1
andreiolaru <andrei.olaru@gmail.com>
2026-04-14 20:15:18 +0300

    Split homepage latest fonts into free and commercial

--- template-filter-all.php
+++ template-filter-all.php
@@ -18,6 +18,7 @@ $kreativ_fa_icons = [
 $font_filters = kreativ_get_font_filters();
 $active_font_filter = kreativ_get_active_font_filter( $font_filters );
 $active_font_filter_config = $font_filters[ $active_font_filter ];
+$free_fonts_slugs = function_exists( 'kreativ_get_free_fonts_category_slugs' ) ? kreativ_get_free_fonts_category_slugs() : array();
 
 ?>
 
@@ -111,22 +112,69 @@ $active_font_filter_config = $font_filters[ $active_font_filter ];
      LATEST BY CATEGORY
 ===================================================== -->
 <?php
-$home_sections = [
-    'fonts' => $active_font_filter_config['title'],
-];
+$home_sections = array();
 
-foreach ( $home_sections as $slug => $title ) :
-    $icon = $kreativ_fa_icons[ $slug ] ?? '';
-    $font_tax_query = array_merge(
-        [
-            [
+if ( 'latest' === $active_font_filter && $free_fonts_slugs ) {
+    $home_sections[] = array(
+        'slug'       => 'fonts',
+        'title'      => 'Latest Commercial Fonts',
+        'view_all'   => home_url( '/fonts' ),
+        'show_filters' => true,
+        'tax_query'  => array(
+            array(
                 'taxonomy' => 'category',
-                'field' => 'slug',
-                'terms' => [ $slug ],
-            ],
-        ],
-        $active_font_filter_config['tax_query']
+                'field'    => 'slug',
+                'terms'    => array( 'fonts' ),
+            ),
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'slug',
+                'terms'    => $free_fonts_slugs,
+                'operator' => 'NOT IN',
+            ),
+        ),
     );
+
+    $home_sections[] = array(
+        'slug'       => 'fonts',
+        'title'      => 'Latest Free Fonts',
+        'view_all'   => add_query_arg( 'font_filter', 'free', home_url( '/fonts' ) ),
+        'show_filters' => false,
+        'tax_query'  => array(
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'slug',
+                'terms'    => array( 'fonts' ),
+            ),
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'slug',
+                'terms'    => $free_fonts_slugs,
+            ),
+        ),
+    );
+} else {
+    $home_sections[] = array(
+        'slug'       => 'fonts',
+        'title'      => $active_font_filter_config['title'],
+        'view_all'   => add_query_arg( 'font_filter', $active_font_filter, home_url( '/fonts' ) ),
+        'show_filters' => true,
+        'tax_query'  => array_merge(
+            array(
+                array(
+                    'taxonomy' => 'category',
+                    'field'    => 'slug',
+                    'terms'    => array( 'fonts' ),
+                ),
+            ),
+            $active_font_filter_config['tax_query']
+        ),
+    );
+}
+
+foreach ( $home_sections as $section ) :
+    $slug = $section['slug'];
+    $icon = $kreativ_fa_icons[ $slug ] ?? '';
     ?>
     <div class="container kreativ-section kreativ-section-<?php echo esc_attr( $slug ); ?>">
 
@@ -135,51 +183,45 @@ foreach ( $home_sections as $slug => $title ) :
                 <?php if ( $icon ) : ?>
                     <i class="<?php echo esc_attr( $icon ); ?>"></i>
                 <?php endif; ?>
-                <?php echo esc_html( $title ); ?>
+                <?php echo esc_html( $section['title'] ); ?>
             </h2>
-            <?php
-            $section_url = 'fonts' === $slug ? home_url( '/fonts' ) : home_url( '/category/' . $slug );
-            $section_url = add_query_arg( 'font_filter', $active_font_filter, $section_url );
-            ?>
-            <a href="<?php echo esc_url( $section_url ); ?>" class="kf-view-all">View All &rsaquo;</a>
+            <a href="<?php echo esc_url( $section['view_all'] ); ?>" class="kf-view-all">View All &rsaquo;</a>
         </div>
 
-        <div class="kreativ-font-filter-bar">
-            <?php foreach ( $font_filters as $filter_slug => $filter_config ) : ?>
-                <a href="<?php echo esc_url( add_query_arg( 'font_filter', $filter_slug, get_permalink() ) ); ?>" class="kreativ-font-filter <?php echo $active_font_filter === $filter_slug ? 'active' : ''; ?>">
-                    <?php echo esc_html( $filter_config['label'] ); ?>
-                </a>
-            <?php endforeach; ?>
-        </div>
+        <?php if ( ! empty( $section['show_filters'] ) ) : ?>
+            <div class="kreativ-font-filter-bar">
+                <?php foreach ( $font_filters as $filter_slug => $filter_config ) : ?>
+                    <a href="<?php echo esc_url( add_query_arg( 'font_filter', $filter_slug, get_permalink() ) ); ?>" class="kreativ-font-filter <?php echo $active_font_filter === $filter_slug ? 'active' : ''; ?>">
+                        <?php echo esc_html( $filter_config['label'] ); ?>
+                    </a>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <div class="row">
             <?php
-            $query = new WP_Query( [
+            $query = new WP_Query( array(
                 'posts_per_page'         => 24,
                 'post_status'            => 'publish',
                 'ignore_sticky_posts'    => true,
-                'no_found_rows'          => true, // perf
+                'no_found_rows'          => true,
                 'update_post_term_cache' => false,
                 'update_post_meta_cache' => false,
-                'orderby'                => $active_font_filter_config['orderby'],
+                'orderby'                => 'latest' === $active_font_filter ? 'date' : $active_font_filter_config['orderby'],
                 'order'                  => 'DESC',
-                'tax_query'              => $font_tax_query,
-            ] );
+                'tax_query'              => $section['tax_query'],
+            ) );
 
             if ( $query->have_posts() ) :
                 while ( $query->have_posts() ) :
                     $query->the_post();
-                    ?>
-                    <?php
                     kreativ_render_font_card(
                         array(
-                            'post_id'      => get_the_ID(),
-                            'badge_text'   => $kreativ_category_labels[ $slug ],
-                            'badge_slug'   => $slug,
+                            'post_id'    => get_the_ID(),
+                            'badge_text' => $kreativ_category_labels[ $slug ],
+                            'badge_slug' => $slug,
                         )
                     );
-                    ?>
-                <?php
                 endwhile;
             else :
                 ?>
